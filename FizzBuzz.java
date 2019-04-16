8baae61de1235d4381be9f8706502b009bc826a3
Naveen <nandharam.naveen@gmail.com>
2019-04-16 11:05:07 +0800

    Here attaching java script and java files

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FizzBuzz{
+
+public static void main(String args[]){
+
+
+for (int i=0;i<=100;i++){
+ if (i%3 == 0 && i%5==0)
+  System.out.println("FiZZBuzz");
+ else if (i%3 == 0)
+  System.out.println("FiZZ");
+  else if(i%5 ==0)
+  System.out.println("Buzz");
+ else
+  System.out.println(i);
+}
+
+}
+}
